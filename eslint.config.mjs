6ef8b04f296fb363f78016347b8fbc510362e115
unknown <suenming.kung@gmail.com>
2025-08-11 16:12:39 -0400

    Shopping cart on every page

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,5 @@
-import js from "@eslint/js";
-import globals from "globals";
-import { defineConfig } from "eslint/config";
+import js from "@eslint/js"
+import globals from "globals"
+import { defineConfig } from "eslint/config"
 
-export default defineConfig([ { plugins: { js }, languageOptions: { globals: { ...globals.browser, ...globals.node } } }, ]);
+export default defineConfig([{ plugins: { js }, languageOptions: { globals: { ...globals.browser, ...globals.node } } }])
