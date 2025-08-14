fcbdb2f0b046f33b51e389f64328f828b67ac367
unknown <suenming.kung@live.com>
2025-08-14 15:48:10 -0400

    Update config.js

--- JS/config.js
+++ JS/config.js
@@ -1,5 +1,5 @@
 // Global configuration for the application
 const config = {
-	apiBaseUrl: "https://127.0.0.1:3000/api",
-	// apiBaseUrl: "https://e-commerceproject-x4gr.onrender.com/api",
+	// apiBaseUrl: "https://127.0.0.1:3000/api",
+	apiBaseUrl: "https://e-commerceproject-x4gr.onrender.com/api",
 }
