f1796f3041fede064c4290c97af2b37155ed3bdd
unknown <suenming.kung@live.com>
2025-08-16 11:31:16 -0400

    Update api-config.js

--- JS/api-config.js
+++ JS/api-config.js
@@ -1,5 +1,8 @@
-// Global configuration for the application
+/* Automatically select the API URL based on the current hostname
+	If it's '127.0.0.1', 'localhost', or an empty string (local file), use the local URL
+	Otherwise, use the online URL */
 const config = {
-	// apiBaseUrl: "https://127.0.0.1:3000/api",
-	apiBaseUrl: "https://e-commerceproject-x4gr.onrender.com/api",
+	apiBaseUrl: window.location.hostname === "127.0.0.1" || window.location.hostname === "localhost" || window.location.hostname === ""
+	? "https://127.0.0.1:3000/api"
+	: "https://e-commerceproject-x4gr.onrender.com/api"
 }
