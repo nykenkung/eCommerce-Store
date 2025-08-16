7a531e4722dc98cfa3ac532614a6bb9f423ae618
unknown <suenming.kung@live.com>
2025-08-16 05:28:50 -0400

    Show slideshow dots on index.html

--- JS/index.js
+++ JS/index.js
@@ -13,6 +13,7 @@ function closeBanner() {
 // Slideshow
 let currentHeroIndex = 0
 const heroImages = ["../clothing/hero.png", "../clothing/hero1.jpg", "../clothing/hero2.jpg", "../clothing/hero3.jpg", "../clothing/hero4.jpg", "../clothing/hero5.jpg", "../clothing/hero6.jpg", "../clothing/hero7.jpg", "../clothing/hero8.jpg", "../clothing/hero9.jpg"]
+const heroDotsContainer = document.getElementById("hero-dots")
 
 // Manual arrow clicks
 let heroInterval
@@ -23,6 +24,10 @@ function handleArrowClick(n) {
 }
 
 function changeHero(n) {
+	// Remove active class from current dot
+	const oldDot = heroDotsContainer.children[currentHeroIndex]
+	if (oldDot) oldDot.classList.remove("active")
+
 	currentHeroIndex += n
 	if (currentHeroIndex < 0) {
 		currentHeroIndex = heroImages.length - 1
@@ -31,9 +36,33 @@ function changeHero(n) {
 		currentHeroIndex = 0
 	}
 	document.getElementById("hero").style.backgroundImage = `url('${heroImages.at(currentHeroIndex)}')`
+
+	// Add active class to new dot
+	const newDot = heroDotsContainer.children[currentHeroIndex]
+	if (newDot) newDot.classList.add("active")
 }
+
 function startSlideshow() {
 	heroInterval = setInterval(() => changeHero(1), 4000) // 4 seconds timer to play next slide
+}
+
+// Create and initialize dots
+function createHeroDots() {
+	heroImages.forEach((_, index) => {
+		const dot = document.createElement("div")
+		dot.classList.add("dot")
+		dot.onclick = () => {
+			clearInterval(heroInterval)
+			currentHeroIndex = index - 1 // Subtract 1 because changeHero adds 1
+			changeHero(1)
+			startSlideshow()
+		}
+		heroDotsContainer.appendChild(dot)
+	})
+	// Activate the first dot on load
+	if (heroDotsContainer.children[currentHeroIndex]) {
+		heroDotsContainer.children[currentHeroIndex].classList.add("active")
+	}
 }
 
 // On page load
@@ -53,6 +82,9 @@ window.addEventListener("DOMContentLoaded", () => {
 		const img = new Image()
 		img.src = src
 	})
+
+	createHeroDots() // Creates the dots before the slideshow starts
+
 	// Initialize hero image
 	document.getElementById("hero").style.backgroundImage = `url('${heroImages.at(currentHeroIndex)}')`
 	startSlideshow()
