b799cad6b95af6cd1a31ea80ffa6b64b33a68bf5
unknown <suenming.kung@live.com>
2025-08-18 14:58:30 -0400

    Fix major configuration error

--- JS/cart-preview.js
+++ JS/cart-preview.js
@@ -167,7 +167,7 @@ function changeQty(index, change) {
 
 // Main Initialization Sequence
 document.addEventListener("DOMContentLoaded", () => {
-	fetch(`${config.product}`)
+	fetch(`${product}`) // fetch("products.json") // Local file
 		.then((response) => {
 			if (!response.ok) throw new Error("Network response occurs error!")
 			return response.json()
